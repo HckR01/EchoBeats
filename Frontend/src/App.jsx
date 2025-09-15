d0c03e3e2b5b46f1ead26f3d18655edd2e562507
HckR01 <139985239+HckR01@users.noreply.github.com>
2025-09-15 22:46:46 +0530

    add new custom styles

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./index.css";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold font-zentry text-violet-300">Hello</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden ">
+      <Hero></Hero>
     </main>
   );
 }
